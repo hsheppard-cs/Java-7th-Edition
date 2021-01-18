0eae694f62732fff9b43508fe945dade1a49f6c8
hsheppard-cs <sheppardhamilton01@gmail.com>
2021-01-18 15:14:41 -0500

    Made changes

--- Chapter 3 - Decision Structures/chapterThreeChallengeTwo.java
+++ Chapter 3 - Decision Structures/chapterThreeChallengeTwo.java
@@ -2,9 +2,16 @@ import java.util.Scanner;
 
 class chapterThreeChallengeTwo
 {
+   /**
+   * Version 1
+   * Magic Date
+   * After the user has entered a month (in numeric form), a day, and a two-digit
+   * year, the program will determine whether the month times the day is equal to the two-digit year.
+   * If so, the message "Magic Date" will display, if not, the message "Not a magic date" will display.
+   */
    public static void main(String[] args)
    {
-      int month, day, year;
+      int month, day, year; // User-entered information
       Scanner keyboard = new Scanner(System.in);
       
       System.out.print("Enter a month (In numeric form): ");
@@ -14,6 +21,7 @@ class chapterThreeChallengeTwo
       System.out.print("Enter a two-digit year: ");
       year = keyboard.nextInt();
       
+      // Utilizes [width] of the format specifier
       System.out.printf("Date Entered:%3d/%d/%d\n",month,day,year);
       
       if ((month * day) == year)
